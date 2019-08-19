359e28f6b763323626de5b16b0f6eb8bfb740ad4
ZDragon1996 <z841063918@gmail.com>
2019-08-19 09:44:48 -0400

    Add condition check for add and remove

--- Entity/Profile.cs
+++ Entity/Profile.cs
@@ -41,43 +41,101 @@ namespace Entity
 
         public Profile(User user)
         {
+            //profile
             ProfileUser = user;
             ProfileUserID = user.ID;
+            //followers
             Followers = new List<User>();
-            FollowersCount = Followers.Count;
-            Following = new List<User>();
-            FollowingCount = Following.Count;
+            FollowersCount = TotalFollowerCount();
+            //block list
             BlockList = new List<User>();
-            BlockCount = BlockList.Count;
+            BlockCount = TotalBlockListCount(); ;
+
             isActive = true;
         }
 
         //Have to update following user Followers and FollowersCount in profile  
         public void Follow(User user)
         {
-            Following.Add(user);
-            FollowingCount = Followers.Count;
+            if (!FollowerCountIsFull())
+            {
+                Following.Add(user);
+            }
+            else
+            {
+                // Follower count is full int max
+            }
+           
         }
 
         //Have to update following user Followers and FollowersCount in profile  
         public void UnFollow(User user)
         {
-            Following.Remove(user);
-            FollowingCount = Followers.Count;
+            if (!FollowerCountIsEmpty())
+            {
+                Following.Remove(user);
+            }
+            else
+            {
+                //follower count is empty
+            }
+           
         }
+        public int TotalFollowerCount()
+        {
+            return FollowingCount = Followers.Count;
+        }
+
+        public bool FollowerCountIsEmpty()
+        {
+            return FollowersCount == 0;
+        }
+
+        public bool FollowerCountIsFull()
+        {
+            return FollowersCount == int.MaxValue;
+        }
+
+        public int TotalBlockListCount()
+        {
+            return BlockCount = BlockList.Count;
+        }
+
+        public bool BlockListIsEmpty()
+        {
+            return BlockCount == 0;
+        }
+
+        public bool BlockListIsFull()
+        {
+            return BlockCount == int.MaxValue;
+        }
+
 
         public void Block(User user)
         {
-            BlockList.Add(user);
-            BlockCount = BlockList.Count;
+            if (!BlockListIsFull())
+            {
+                BlockList.Add(user);
+            }
+            else
+            {
+                //Block List is full int max
+            }
+           
         }
 
         public void UnBlock(User user)
         {
-            BlockList.Remove(user);
-            BlockCount = BlockList.Count;
+            if (!BlockListIsEmpty())
+            {
+                BlockList.Remove(user);
+            }
+            else
+            {
+                //Block List count is empty
+            }
         }
-
 
     }
 }
