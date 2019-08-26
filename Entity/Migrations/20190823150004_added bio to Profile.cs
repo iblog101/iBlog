663a6497c9ee54daf25299661e374b08bfed6181
Luis Concepcion Bido <laconcepcionbido@gmail.com>
2019-08-26 10:25:01 -0400

    added migrations

---
+++ Entity/Migrations/20190823150004_added bio to Profile.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entity.Migrations
+{
+    public partial class addedbiotoProfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Profiles",
+                maxLength: 250,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Profiles");
+        }
+    }
+}
