2732f8858933251120b5905bb1f18240032c7510
Luis Concepcion Bido <laconcepcionbido@gmail.com>
2019-10-19 09:23:27 -0700

    Added Migrations

---
+++ Entity/Migrations/20191019162130_Updated Users Table.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entity.Migrations
+{
+    public partial class UpdatedUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sex",
+                table: "Users",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sex",
+                table: "Users");
+        }
+    }
+}
